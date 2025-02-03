1c983406765cb0568e869de1d5ccb6ba05fd1b8b
Sagar <sagarsvss5838@gmail.com>
2025-02-03 09:10:43 +0530

    this is my commit to main branch

---
+++ calculator.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+##############
+# Author: Sagar
+# Date: 03/01/2025
+# Title: This is file contains the calculator code
+##############
+
+
+# Addition functionlaity
+
+#Addition of two numbers
+x1=a+b
+
+#addition of three numbers
+x2=a+b+c
+
+
