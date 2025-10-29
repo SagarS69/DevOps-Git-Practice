8221b1fe08a6397f75145b66f276b1395f4a78f9
Sagar <sagarsvss5838@gmail.com>
2025-10-29 10:24:21 +0530

    clearing previous codes

--- calculator.sh
+++ calculator.sh
@@ -2,7 +2,7 @@
 
 ##############
 # Author: Sagar
-# Date: 03/01/2025
+# Date: 29/10/2025
 # Title: This is file contains the calculator code
 ##############
 
@@ -10,32 +10,4 @@
 # Addition functionlaity
 
 #Addition of two numbers
-x1=a+b
 
-<<<<<<< HEAD
-#Addition of three numbers
-x2=a+b+c
-=======
-#addition of three numbers
-#x2=a+b+c
-
-
-
-# Substraction functionality
-
-# Difference of two numbers
-y1=a-b
-
-# Difference of three numbers
-y2=a-b-c
-
->>>>>>> substraction
-
-
-# Division functionality
-
-#Division of two numbers
-z1=a/b
-
-
-# Adding Multiplication functionality
